9ceefcbffcc33ffafeec3b1fabc8cab82eb01498
adryalves <adryellenalves09@gmail.com>
2025-12-20 22:31:15 -0300

    feature: added category controller and auto mapper related to category

---
+++ backend/expense-control-api/Controllers/CategoryController.cs
@@ -0,0 +1,103 @@
+﻿using expense_control_api.DTOs;
+using expense_control_api.Interfaces;
+using expense_control_api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace expense_control_api.Controllers
+{
+    [Route("/api/v1.0/category")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CategoryResponse>> CreateCategory([FromBody] CategoryRequest request)
+        {
+            try
+            {
+                if (!ModelState.IsValid) return BadRequest(ModelState);
+                
+                    var category = await _categoryService.CreateCategory(request);
+
+                if(!category.Success)
+                    return BadRequest(new { error = category.Error });
+
+                return Ok(category.Data);
+             
+
+            }
+
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet("All")]
+        public async Task<ActionResult<CategoryListResponse>> GetAllCategories()
+        {
+            try
+            {
+                var categories = await _categoryService.GetAllCategories();
+
+                if(!categories.Success)
+                    return BadRequest(new { error = categories.Error });
+
+                return Ok(categories.Data.CategoryList);
+            }
+
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CategoryResponse>> GetCategoryById(Guid id)
+        {
+            try
+            {
+                var category = await _categoryService.GetCategoryById(id);
+                if (category == null) return NotFound("Não foi encontrada uma categoria com esse id");
+
+                if(!category.Success)
+                    return BadRequest(new { error = category.Error });
+
+                return Ok(category.Data);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<CategoryResponse>> UpdateCategory(Guid id, CategoryRequest request)
+        {
+            try
+            {
+                var category = await _categoryService.GetCategoryById(id);
+                if (category.Data == null) return NotFound("Não foi encontrada uma categoria com esse id");
+
+                var updatedCategory = await _categoryService.UpdateCategory(id, request);
+
+                if(!updatedCategory.Success)
+                    return BadRequest(new { error = updatedCategory.Error });
+
+                return Ok(updatedCategory);
+            }
+
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
---
+++ backend/expense-control-api/Mappings/CategoryProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using expense_control_api.DTOs;
+using expense_control_api.Models;
+
+namespace expense_control_api.Mappings
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<CategoryRequest, Category>();
+            CreateMap<Category,CategoryResponse>();
+        }
+    }
+}
