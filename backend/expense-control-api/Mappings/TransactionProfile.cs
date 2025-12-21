c5e50829c30c016fa5aa1d1da1b0628859451dd1
adryalves <adryellenalves09@gmail.com>
2025-12-20 22:32:19 -0300

    feature: added auto mapper related to transaction

---
+++ backend/expense-control-api/Mappings/TransactionProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using expense_control_api.DTOs;
+using expense_control_api.Models;
+
+
+namespace expense_control_api.Mappings
+{
+    public class TransactionProfile : Profile 
+    {
+        public TransactionProfile() 
+        {
+            CreateMap<TransactionRequest, Transaction>();
+            CreateMap<Transaction, TransactionResponse>();
+        }
+    }
+}
