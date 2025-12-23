252026f6134e7957ca19624268873378eb85960f
adryalves <adryellenalves09@gmail.com>
2025-12-22 23:14:32 -0300

    feature: added CORS configuration so frontend could access api

--- backend/expense-control-api/Program.cs
+++ backend/expense-control-api/Program.cs
@@ -2,13 +2,13 @@ using expense_control_api.Data;
 using expense_control_api.Interfaces;
 using expense_control_api.Services;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -16,6 +16,22 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new() { Title = "ProjetoControleGastos", Version = "v1" });
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "LocalDevCors",
+        policy =>
+            policy
+                .WithOrigins(
+                    "http://localhost:5173",
+                    "http://127.0.0.1:5173",
+                    "http://localhost:4173",
+                    "http://127.0.0.1:4173"
+                )
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+    );
+});
 
 builder.Services.AddScoped<IPersonService, PersonService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
@@ -36,6 +52,8 @@ builder.Services.AddDbContext<ExpenseControlDbContext>(options =>
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
         options.JsonSerializerOptions.Converters.Add(
             new JsonStringEnumConverter()
         );
@@ -51,7 +69,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
+
+app.UseCors("LocalDevCors");
 
 app.UseAuthorization();
 
